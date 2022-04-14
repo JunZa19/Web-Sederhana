936d5598ee02eeacd5de7e4cf028fba01af8b9f2
JunZa19 <Juniorzaitun@gmail.com>
2022-04-14 19:43:19 +0700

    menambahkan Web.php,v_about.php,v_footer.php,v_header.php,v_index.php

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Junior</li>
+ <li>Nama Belakang : Panderaya</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Cendrawasih VI </li>
+ </ul>
+
+ <h4>Tempat Lahir, Tanggal Lahir</h4>
+ <ul type="none">
+ <li>Tentena, 19 Agustus 1999</li>
+ </ul>
+ <h4>MY HOBBY</h4>
+ <ul type="square">
+ <li>Main Game</li>
+ <li>Makan</li>
+ <li>Tidur</li>
+ </ul>
+</section>
